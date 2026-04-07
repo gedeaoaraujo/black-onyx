9844cde1a7ad0d0ed4199b95bcf8dba849df6cc2
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-04-06 21:49:47 -0300

    Moving the logic to the upper layers.

--- composeApp/src/commonMain/kotlin/com/example/blackonyx/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/example/blackonyx/HomeViewModel.kt
@@ -1,22 +1,33 @@
 package com.example.blackonyx
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class HomeState(
   val notes: List<Note> = listOf()
 )
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel(
+  val repository: HomeRepository = HomeRepository(),
+  val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+): ViewModel() {
 
   var state = MutableStateFlow(HomeState())
     private set
 
   init {
-    val items = mutableListOf<Note>()
-    repeat(10){ items.add(Note()) }
-    state.update { it.copy(notes = items) }
+    getAllNotes()
+  }
+
+  fun getAllNotes() = viewModelScope.launch(ioDispatcher) {
+    val notes = repository.getAllNotes()
+    state.update { it.copy(notes = notes) }
   }
 
 }
