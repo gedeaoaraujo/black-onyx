f7e9a423c537873d8c0f246c40e19f74bf5043a6
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-04-07 13:39:42 -0300

    Improving.

--- composeApp/src/commonMain/kotlin/com/example/blackonyx/domain/Note.kt
+++ composeApp/src/commonMain/kotlin/com/example/blackonyx/domain/Note.kt
@@ -3,6 +3,5 @@ package com.example.blackonyx.domain
 data class Note(
   val title: String = "This is a title",
   val date: String = "06/04/2026 14:00",
-  val description: String = "çal~sdkçalskdãlsd la~çskdlçakslçasçdlkasçdkasçldkasçldasçlkdçaskdçask"+
-      "asldkasldjlaksjdlksajldçkjasdkjaslkjdasljdlkjasdljasldjalksjdlakjsdlkjasldkjalsjdlas"
+  val description: String = "a".repeat(255)
 )
