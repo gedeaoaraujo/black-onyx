7e1cb85c34ceaff8b48c3a9b66851bf752a60b4e
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-04-17 02:36:14 -0300

    Adding backPress navigation dependency.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -42,6 +42,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
+            implementation(libs.navigationevent.compose)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
