c1d223ee8f7219d3156388bce68f01b47b3bf2c8
gedeoaraujo <gedeao.lemobs@gmail.com>
2026-04-19 14:37:27 -0300

    Creating ThemeButton component.

---
+++ composeApp/src/commonMain/kotlin/com/example/blackonyx/components/ThemeButton.kt
@@ -0,0 +1,19 @@
+package com.example.blackonyx.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ThemeButton(onClick: () -> Unit) {
+  IconButton(onClick = onClick) {
+    Icon(
+      contentDescription = "Save Note",
+      imageVector = Icons.Default.Check,
+      tint = MaterialTheme.colorScheme.onPrimary
+    )
+  }
+}
